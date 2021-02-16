9a3854eb8a9d91970fa76205528ad44bcd142952
JulianIlla <jilla23211@columbushs.com>
2021-02-16 08:37:09 -0500

    video2 done commit

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+const before = new Date('February 1 2019 7:30:59');
 const now = new Date();
 
-console.log(now);
-console.log(typeof now);
+const diff = now.getTime() - before.getTime();
+// const diff = now - before;
 
-console.log('getFullYear:', now.getFullYear());
-console.log('getMonth (0-based):', now.getMonth());
-console.log('getDate:', now.getDate());
-console.log('getDay (0-based):', now.getDay());
-console.log('getHours:', now.getHours());
-console.log('getMinutes:', now.getMinutes());
-console.log('getSeconds:', now.getSeconds());
+console.log(diff);
 
-console.log('timestamp:', now.getTime());
+const mins = Math.round(diff / 1000 / 60);
+const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
 
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toLocaleString());
+console.log(`the before date was ${mins} mins ago`);
+console.log(`the before date was ${hours} hours ago`);
+console.log(`the before date was ${days} days ago`);
+
+const timestamp = 1675938474990;
+console.log(new Date(timestamp));
